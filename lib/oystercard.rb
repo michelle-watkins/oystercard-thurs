9537e565b019f1f159db16947953901cc2c3f3da
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-09-27 16:57:04 +0100

    Enabled top up functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,11 @@
 class Oystercard
     attr_reader :balance
+    
     def initialise
         @balance = 0
     end
+
+    def top_up(top_up_amount)
+        @balance += top_up_amount
+    end    
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,19 @@ describe Oystercard do
         expect(subject.initialise).to eq(0)
     end
 
-end  
+    describe '#top_up' do
+
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+  
+      # it "adds an amount to the balance" do
+      #   expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      # end
+
+      it "adds an amount to the balance" do
+        subject.initialise
+        expect(subject.top_up(10)).to eq 10
+      end  
+    end 
+end    
+
+ 
