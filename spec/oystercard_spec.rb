b502fffef4760254e6b2df1bad152c4129acebb5
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-09-28 08:09:37 +0100

    modified spec file

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,18 +6,20 @@ describe Oystercard do
     end
 
     describe '#top_up' do
-
       it { is_expected.to respond_to(:top_up).with(1).argument }
-  
+    end
+    
+    describe '#top_up' do
       # it "adds an amount to the balance" do
+      #   subject.initialise(0)
       #   expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
       # end
 
       it "adds an amount to the balance" do
         subject.initialise(0)
         expect(subject.top_up(10)).to eq 10
-      end  
-    end 
+      end 
+    end
 end    
 
  
