8ed567c708b777330a04cf276c5904b88ecc7cbb
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-09-28 14:52:51 +0100

    added strings to methods for readability

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,16 @@
 class Oystercard
     attr_reader :balance
     
-    def initialise(balance)
-        @balance = balance
+    def initialize
+        @balance = 0
     end
+
+    def check_balance
+        "Your balance is £#{@balance}."
+    end    
 
     def top_up(top_up_amount)
         @balance += top_up_amount
+        "You added a £#{top_up_amount} top-up."
     end    
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,25 +1,18 @@
 require "oystercard"
 
 describe Oystercard do
-    it "intialise card balance" do
-        expect(subject.initialise(0)).to eq(0)
+    it "intialise card balance with 0" do
+        expect(subject.check_balance).to eq("Your balance is £0.")
     end
 
     describe '#top_up' do
       it { is_expected.to respond_to(:top_up).with(1).argument }
-    end
-    
-    describe '#top_up' do
-      # it "adds an amount to the balance" do
-      #   subject.initialise(0)
-      #   expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-      # end
 
-      it "adds an amount to the balance" do
-        subject.initialise(0)
-        expect(subject.top_up(10)).to eq 10
+      it "add an amount to the balance" do
+        expect(subject.top_up(10)).to eq ("You added a £10 top-up.")
       end 
     end
-end    
+end
+    
 
  
